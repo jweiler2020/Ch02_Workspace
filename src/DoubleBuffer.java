dffc8d8297838f7ab7ebaad8f0954c37d600dbe8
Justin Weiler <jweiler2020@cusd.claremont.edu>
2018-02-06 19:42:56 -0800

    added DoubleBuffer.java

---
+++ src/DoubleBuffer.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class DoubleBuffer extends JApplet
+		implements ActionListener
+{
+	public void init()
+	{
+
+	}
+
+	public void actionPerformed(ActionEvent e)
+	{
+
+	}
+}
